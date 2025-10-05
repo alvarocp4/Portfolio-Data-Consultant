1f0732bb9e90e2617144dd38d0846b834a0ffe8c
alvarocp4 <a.carmonapedrajas@gmail.com>
2025-10-05 21:42:44 +0200

    Adapt to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +15,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output:'server',
   adapter: vercel(),
 });
