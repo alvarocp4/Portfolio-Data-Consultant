d2889472b67146d49dc857ba007719bb59bf49e8
alvarocp4 <a.carmonapedrajas@gmail.com>
2025-10-09 01:22:54 +0200

    Update name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel'; 
 
 // https://astro.build/config
 export default defineConfig({
