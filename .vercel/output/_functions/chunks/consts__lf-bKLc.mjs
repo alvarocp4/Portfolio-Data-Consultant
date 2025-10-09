a62eb895334281808c48c80083f35270d4e10036
alvarocp4 <a.carmonapedrajas@gmail.com>
2025-10-09 15:30:33 +0200

    Update FIX

--- .vercel/output/_functions/chunks/consts__lf-bKLc.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "Alvaro Carmona - Portfolio";
-const SITE_DESCRIPTION = "Profesional Portfolio - Data Consultant";
-
-export { SITE_DESCRIPTION as S, SITE_TITLE as a };
--- .vercel/output/_functions/chunks/slug_CBq44NJN.mjs
+++
@@ -1,7 +0,0 @@
-function slugifyTag(input) {
-  return encodeURIComponent(
-    input.trim().toLowerCase().replace(/[^a-z0-9\s-]/g, "").replace(/\s+/g, "-")
-  );
-}
-
-export { slugifyTag as s };
